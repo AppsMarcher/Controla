960f8df76f876e3034344c568a375845e8c8f34a
AppsMarcher <ricardo@marcher.com.br>
2026-06-18 13:19:02 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Controla/',
+  base: '/',
   server: { port: 5173, open: true },
   build: { outDir: 'dist', sourcemap: false }
 });
